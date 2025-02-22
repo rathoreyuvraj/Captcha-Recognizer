13ba386b0d2ac4a288d34ce30f01a2d481e6bc83
Yuvraj singh rathore <78069612+rathoreyuvraj@users.noreply.github.com>
2025-02-22 23:31:58 +0530

    model files commit

---
+++ config.py
@@ -0,0 +1,8 @@
+DATA_DIR = "dataset\\train-images\\train-images"
+CSV_LOC = "dataset\\captcha_data.csv"
+BATCH_SIZE = 8
+IMAGE_WIDTH = 200
+IMAGE_HEIGHT = 50
+NUM_WORKERS = 2
+EPOCHS = 200
+DEVICE = "cpu"
